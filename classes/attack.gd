a4e73bda110b8904317123ebd796a4fcc7b1f981
Alexander Eklund <alexander460@gmail.com>
2024-09-10 09:14:28 +0200

    feat: add hit animation tree and connect to hurtbox component

--- classes/attack.gd
+++ classes/attack.gd
@@ -3,3 +3,4 @@ class_name Attack
 # Just a data class, used for passing attack information
 
 var damage: int
+var knockback: float
--- scripts/components/hitbox_component.gd
+++ scripts/components/hitbox_component.gd
@@ -2,9 +2,17 @@ class_name HitboxComponent
 extends Area2D
 
 @export var health_component : HealthComponent
-@onready var animated_sprite = $"../AnimatedSprite2D"
+@onready var animation_tree: AnimationTree = $"../AnimationTree"
 
 func damage(attack: Attack):
 	if health_component:
 		health_component.damage(attack)
-		animated_sprite.play("hit")
+		if animation_tree:
+			animation_tree["parameters/conditions/is_hit"] = true
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "hit":
+		if animation_tree:
+			animation_tree["parameters/conditions/is_hit"] = false
+	
